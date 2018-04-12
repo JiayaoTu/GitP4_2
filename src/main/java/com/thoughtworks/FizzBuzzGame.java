6804940843b7dc65d019a8ca0aed55a7cc873742
Kingtanna <1435462374@qq.com>
2018-04-12 17:47:15 +0800

    first commit

--- src/main/java/com/thoughtworks/FizzBuzzGame.java
+++ src/main/java/com/thoughtworks/FizzBuzzGame.java
@@ -7,9 +7,100 @@ import java.util.List;
 public class FizzBuzzGame {
     private List<String> results = new ArrayList<>();
 
-    public void start(int count) {
+    private int num1=3, num2=5, num3=7;
 
+    public void start(int count) {
+        for (int n = 1; n <= count; n++) {
+            String s=String.valueOf(n);
+            if(!Return_Fizz_when_have_3(n).equals(s))
+            {
+                results.add(Return_Fizz_when_have_3(n));
+              //  continue;
+            }else if(!Return_FizzBuzzWhizz_when_have_357(n).equals(s))
+            {
+                results.add(Return_FizzBuzzWhizz_when_have_357(n));
+                //continue;
+
+            }else if(!Return_TwoOf_FizzBuzzWhizz_when_have_TwoOf_357(n).equals(s))
+            {
+                results.add(Return_TwoOf_FizzBuzzWhizz_when_have_TwoOf_357(n));
+               // continue;
+
+
+            }else if(!Return_OneOf_FizzBuzzWhizz_when_have_OneOf_357(n).equals(s))
+            {
+                System.out.println(Return_OneOf_FizzBuzzWhizz_when_have_OneOf_357(n));
+
+                results.add(Return_OneOf_FizzBuzzWhizz_when_have_OneOf_357(n));
+              //  continue;
+
+            }else{
+                results.add(s);
+            }
+
+
+        }
     }
+
+
+    public String Return_Fizz_when_have_3(int n)
+    {
+        String str="Fizz";
+        if(String.valueOf(n).indexOf(num1 + 48) != -1)
+        {
+            return str;
+        }else{
+            return String.valueOf(n);
+        }
+    }
+
+    public String Return_FizzBuzzWhizz_when_have_357(int n)
+    {
+        String str="FizzBuzzWhizz";
+        if(n % num1 == 0 && n % num2 == 0 && n % num3 == 0)
+        {
+            return str;
+        }else{
+            return String.valueOf(n);
+        }
+    }
+
+    public String Return_TwoOf_FizzBuzzWhizz_when_have_TwoOf_357(int n)
+    {
+        String str12="FizzBuzz";
+        String str13="FizzWhizz";
+        String str23="BuzzWhizz";
+        if(n % num1 == 0 && n % num2 == 0)
+        {
+            return str12;
+        } else if(n % num2 == 0 && n % num3 == 0){
+            return str23;
+        }else if(n % num1 == 0 && n % num3 == 0){
+            return str13;
+        }else{
+            return String.valueOf(n);
+        }
+    }
+    public String Return_OneOf_FizzBuzzWhizz_when_have_OneOf_357(int n)
+    {
+        String str1="Fizz";
+        String str2="Buzz";
+        String str3="Whizz";
+        if(n % num1 == 0 )
+        {
+            return str1;
+
+        } else if(n % num2 == 0 ){
+            return str2;
+        }else if(n % num3 == 0 ){
+            return str3;
+        }else{
+            return String.valueOf(n);
+        }
+    }
+
+
+
 
     public List<String> getResults() {
         return results;
--- src/test/java/com/thoughtworks/FizzBuzzGameTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzGameTest.java
@@ -1,10 +1,74 @@
 package com.thoughtworks;
 
 import org.junit.Test;
+import java.util.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 public class FizzBuzzGameTest {
+
+
     @Test
     public void should_return_Fizz_when_have_3() {
+        int n1=13;
+
+        FizzBuzzGame game=new FizzBuzzGame();
+        assertThat(game.Return_Fizz_when_have_3(n1),is("Fizz"));
+
+//
+    }
+
+
+    @Test
+    public void should_Return_FizzBuzzWhizz_when_have_357()
+    {
+        int n1=105;
+
+        FizzBuzzGame game=new FizzBuzzGame();
+        assertThat(game.Return_FizzBuzzWhizz_when_have_357(n1),is("FizzBuzzWhizz"));
+    }
+
+    @Test
+    public void should_Return_TwoOf_FizzBuzzWhizz_when_have_TwoOf_357()
+
+    {
+        int n1=15;
+        int n2=21;
+        int n3=35;
+        FizzBuzzGame game=new FizzBuzzGame();
+        assertThat(game.Return_TwoOf_FizzBuzzWhizz_when_have_TwoOf_357(n1),is("FizzBuzz"));
+        assertThat(game.Return_TwoOf_FizzBuzzWhizz_when_have_TwoOf_357(n2),is("FizzWhizz"));
+        assertThat(game.Return_TwoOf_FizzBuzzWhizz_when_have_TwoOf_357(n3),is("BuzzWhizz"));
+
+    }
+
+    @Test
+    public void should_Return_OneOf_FizzBuzzWhizz_when_have_OneOf_357()
+    {
+        //int n1=9;
+        int n2=5;
+        //int n3=28;
+        FizzBuzzGame game=new FizzBuzzGame();
+       // assertThat(game.Return_OneOf_FizzBuzzWhizz_when_have_OneOf_357(n1),is("Fizz"));
+        assertThat(game.Return_OneOf_FizzBuzzWhizz_when_have_OneOf_357(n2),is("Buzz"));
+       // assertThat(game.Return_OneOf_FizzBuzzWhizz_when_have_OneOf_357(n3),is("Whizz"));
+    }
+
+
+
+    @Test
+    public void should_return_Results() {
+       int n1=11;
+        int n2=4;
+        String []str1={"1","2","Fizz","4","Buzz","Fizz","Whizz","8","Fizz","Buzz","11"};
+        String []str2={"1","2","Fizz","4"};
+        List<String> res=Arrays.asList(str1);
+        String sss=res.toString();
+        FizzBuzzGame game=new FizzBuzzGame();
+        game.start(n1);
+        String ss=game.getResults().toString();
+        assertThat(ss,is(sss));
+        //assertThat(game.getResults,is("Fizz"));
 
     }
 }
